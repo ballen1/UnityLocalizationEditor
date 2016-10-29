25d2fe188b878acc44705c4f207b12e499a241c1
Brandon Allen <brandonreallen@gmail.com>
2016-10-29 00:44:56 -0400

    Setting up localization in scene

---
+++ Tools2/Assets/Editor/LocalizationManagerEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(LocalizationManager))]
+public class LocalizationManagerEditor : Editor {
+
+	SerializedProperty translationUnit;
+	SerializedProperty languageUnit;
+
+	void OnEnable() {
+		translationUnit = serializedObject.FindProperty ("selectedTranslationUnit");
+	}
+
+	public override void OnInspectorGUI() {
+
+		EditorGUILayout.PropertyField (translationUnit);
+
+		TranslationUnit tu = (TranslationUnit)translationUnit.objectReferenceValue;
+
+		string[] languages = tu.getAvailableLanguages ();
+
+		for (int i = 0; i < languages.Length; i++) {
+			
+		}
+
+	}
+
+}
--- Tools2/Assets/LocalizationEditor/LocalizationManager.cs
+++ Tools2/Assets/LocalizationEditor/LocalizationManager.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 public class LocalizationManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public TranslationUnit selectedTranslationUnit;
+	public LanguageUnit selectedLanguageUnit;
+
+	void Start() {
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
